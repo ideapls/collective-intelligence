ef1379cc989cdab7c64a5d65306e44f02d65f1ef
Igor Molina <igordsmolina@gmail.com>
2022-12-05 22:10:40 -0300

    Removendo código de teste

--- dataManager.py
+++ dataManager.py
@@ -4,14 +4,6 @@ from connection import get_database
 dbname = get_database()
 collection_name = dbname["Order"]
 
-"""order = {
-    "weight": 14,
-    "number": 0,
-    "items": ["hamburger", "milkshake"]
-}"""
-
-#collection_name.insert_one(order)
-
 
 def insert_order(weight, items):
     order = {
